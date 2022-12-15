f1b3e71259db85977fc6979bbe10f6d00725d136
Caleb <calebbourbonnais1@gmail.com>
2022-12-15 15:52:07 -0700

    added error check for valid directory, removed debug line

--- PacketGen.py
+++ PacketGen.py
@@ -7,7 +7,7 @@ from PacketParser import PacketParser
 from Utils import PacketType
 
 if __name__ == "__main__":
-    #Parse the command line arguments
+    # Parse the command line arguments
     arg_parser = argparse.ArgumentParser(description="Packet Generator")
     arg_parser.add_argument(
         "-d",
@@ -25,14 +25,16 @@ if __name__ == "__main__":
     )
     args = arg_parser.parse_args()
 
-    for file in os.listdir(args.directory):
-        if file[-4:] == ".csv":
-            if "CAN" in file:
-                parser = PacketParser(file)
-                parser.parse('CAN')
-                CGen(parser.get_sections(PacketType.CAN)).generate(args.output)
-            if "TEL" in file:
-                parser = PacketParser(file)
-                parser.parse('TEL')
-                TypescriptGen(parser.get_sections(PacketType.TEL)).generate(args.output)
-                
+    if os.path.isdir(args.directory):
+        for file in os.listdir(args.directory):
+            if file[-4:] == ".csv":
+                if "CAN" in file:
+                    parser = PacketParser(file)
+                    parser.parse('CAN')
+                    CGen(parser.get_sections(PacketType.CAN)).generate(args.output)
+                if "TEL" in file:
+                    parser = PacketParser(file)
+                    parser.parse('TEL')
+                    TypescriptGen(parser.get_sections(PacketType.TEL)).generate(args.output)
+    else:
+        raise ValueError("Invalid directory chosen")
--- PacketParser.py
+++ PacketParser.py
@@ -9,9 +9,8 @@ class PacketParser:
     def __init__(self, filename):
         self.filename = filename
         self.sections = []
-    
+
     def _parse_row(self, row, dtype):
-        print(dtype)
         if row[0] != "":
             if dtype == "CAN":
                 self.sections.append(PacketSection(row[0], PacketType.CAN))
@@ -26,7 +25,7 @@ class PacketParser:
             elif dtype == "TEL":
                 self.sections[-1].add_packet(TELPacket(row[1]))
         self.sections[-1].get_last_packet().add_field(row)
-        
+
     def parse(self, dtype):
         with open(self.filename) as f:
             reader = csv.reader(f)
