b72bfc3e0aca118e7c6c5c0934baaf7855216ca6
Mason Bullock <mason.bullock@ucalgary.ca>
2024-10-18 22:58:24 -0600

    formatted into class

--- csvParser.py
+++ csvParser.py
@@ -2,65 +2,73 @@ import pandas as pd
 import pprint
 import json
 
-def castVal(val):
-    if val.lower() in ['true', 'false']:
-        return val.lower() == 'true'
-    try:
-        return int(val)
-    except ValueError:
-        return val
+### Class for parsing a spreadsheet file and returning a dictionary of the data
+class SpreadsheetParser:
 
-def genDetailsDict(cellString):
-    details = {}
-    commaSplitVals = cellString.split(",")
-    for commaSplitVal in commaSplitVals:
-        key, val = commaSplitVal.split(":")
-        details[key] = castVal(val)
+    ## CONSTRUCTOR - takes in a filepath to the spreadsheet file
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-    return details
+    ## parse - reads the spreadsheet file and returns a dictionary of the data
+    # returns - dictionary of the data or status code -69 if an error occurs
+    def parse(self):
+        res = {}
+        currPacket = ""
 
+        try:
+            df = pd.read_excel(self.filepath)
 
-file = "test.xlsx"
+            # Iterate through the rows of the spreadsheet and populate res dictionary
+            for index, row in df.iterrows():
+                if pd.isna(row["Name"]): # skip null rows (no name)
+                    continue
 
-res = {}
-currPacket = ""
+                if pd.isna(row["Section"]): #if no section value then add to current packet otherwise create new packet
+                    details = None
 
-try:
-    df = pd.read_excel(file)
-    print("opening")
+                    # If there are details, generate a dictionary of them
+                    if not pd.isna(row["Details"]):
+                        details = self.__genDetailsDict(row["Details"])
 
-    # print(df)
-    for index, row in df.iterrows():
-        # if section is not NaN - new packet
-        if pd.isna(row["Name"]):
-            continue
+                    #populate current packet with the row data
+                    res[currPacket].append({"Name": row["Name"].replace(" ", ""),
+                                            "Offset": row["Offset"],
+                                            "Type": row["Type"],
+                                            "Unit": None if pd.isna(row["Units"]) else row["Units"],
+                                            "Detail": details})
+                else: 
+                    # new packet 
+                    currPacket = row["Section"]
+                    res[currPacket] = [{
+                        "Name": row["Name"].replace(" ", ""),
+                        "Offset": row["Offset"], 
+                        "Type": row["Type"],
+                        "Unit": None if pd.isna(row["Units"]) else row["Units"],
+                        "Detail": self.__genDetailsDict(row["Details"])
+                    }]
+            return res
+        
+        except FileNotFoundError:
+            print("Come on now, pick a file that exists")
+            return -69
+        except Exception as e:
+            print(f"You've done ****ed up - {e}")
+            return -69
 
-        if pd.isna(row["Section"]):
-            details = None
-            if not pd.isna(row["Details"]):
-                details = genDetailsDict(row["Details"])
-                    
-            res[currPacket].append({"Name": row["Name"].replace(" ", ""),
-                                    "Offset": row["Offset"],
-                                    "Type": row["Type"],
-                                    "Unit": None if pd.isna(row["Units"]) else row["Units"],
-                                    "Detail": details})
-        else:
-            currPacket = row["Section"]
-            print(row)
-            res[currPacket] = [{
-                "Name": row["Name"].replace(" ", ""),
-                "Offset": row["Offset"], 
-                "Type": row["Type"],
-                "Unit": None if pd.isna(row["Units"]) else row["Units"],
-                "Detail": genDetailsDict(row["Details"])
-            }]
-    pprint.pprint(res)
-
-    with open("output.json", "w") as f:
-        json.dump(res, f, indent=4)
-except FileNotFoundError:
-    print("Not found") 
-except Exception as e:
-    print(f"Error occured: {e}")
-
+    ## __genDetailsDict - generates a dictionary of the details from string in format "key1:val,key2:val,..."
+    def __genDetailsDict(self, cellString):
+        details = {}
+        commaSplitVals = cellString.split(",")
+        for commaSplitVal in commaSplitVals:
+            key, val = commaSplitVal.split(":")
+            details[key] = self.__castVal(val)
+        return details
+    
+    ## __castVal - casts the value to the appropriate type (int, bool, or string)
+    def __castVal(self, val):
+        if val.lower() in ['true', 'false']:
+            return val.lower() == 'true'
+        try:
+            return int(val)
+        except ValueError:
+            return val
