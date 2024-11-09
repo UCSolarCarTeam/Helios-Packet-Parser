f2984d60ba2a3df7c1b07560817637a7658f6200
jtran217 <jktran1998@gmail.com>
2024-11-09 11:06:03 -0700

    Just in case

--- DataLayer/DataLayer.py
+++ DataLayer/DataLayer.py
@@ -1,36 +1,25 @@
 import os
-import testData 
 import json
 
 class DataLayer:
-    def __init__(self, json=testData.data):
-        self.basicHeader = '#pragma once \n#include <qtMethods.h>\n'
+    def __init__(self, json):
         self.parsedData = json
         # Make DataLayer Folder
-        self.directory_name = "DataLayer"
+        self.directory = "Output/DataLayer"
         self.current_directory = os.getcwd()
         # Generate Folder path 
-        self.new_directory_path = os.path.join(self.current_directory, self.directory_name)
+        self.new_directory_path = os.path.join(self.current_directory, self.directory)
         # Make folder if not exist 
         os.makedirs(self.new_directory_path, exist_ok=True)
 
-    def genQMethods(self):
-        fileName = "qtMethods.h"
-        fullPath = os.path.join(self.new_directory_path,fileName)
-        with open(fullPath, 'w') as file:
-            file.write("#include <QObject>\n")
-            file.write("#include <QPair>\n")
-            file.write("#include <QString>\n")
-            file.write("#include <QScopedArrayPointer>\n")
-            file.close()
 
     def determineType(self,attribute):
+        # If the Unit is boolean default to that as type
+        if (attribute["Unit"] == "Boolean"):
+                return "bool"
         if(attribute["Type"] == "short uint"):
             return "unsigned short"
         elif (attribute["Type"] == "uchar"):
-            # This could be simpler and have the csv simply indicate if they want boolean type.
-            if (attribute["Unit"] == "Boolean"):
-                return "bool"
             return "unsigned char"
         elif (attribute["Type"] == "uint"):
             return "unsigned int"
@@ -39,78 +28,75 @@ class DataLayer:
         elif (attribute["Type"] == "float"):
             return "float"
         
-    # def generateInterfaceForMultiUnits(self,packet):
-    #     packetName = list(packet.keys())[0]
-    #     packetData = packet[packetName]
-    #     # Make the folder Name
-    #     dataFolderNamePath = os.path.join(self.new_directory_path,packetName+"Data")
-    #     os.makedirs(dataFolderNamePath,exist_ok=True)
-    #     # Make the data interface file
-    #     headerFileName = "I_{packetName}Data.h".format(packetName=packetName)
-    #     filePath = os.path.join(dataFolderNamePath,headerFileName)
-    #     with open(filePath, "w") as file:
-    #         file.write("#pragma once\n")
-    #         file.write('#include "../qtMethods.h"\n')
-    #         file.write('#include "I_{packetName}Unit.h"\n'.format(packetName = packetName)) 
-    #         file.write("class I_{packetName}Data:public QObject \n{{\nQ_OBJECT\n".format(packetName=packetName))
-    #         file.write("public:\n")  
-    #         # Deconstructor
-    #         file.write("\tvirtual ~I_{packetName}Data() {{}};\n".format(packetName=packetName))
-    #         # Standard 2 getters among multiple unit data packet.
-    #         file.write("\tvirtual unsigned char getNumberOfUnits() const = 0;\n")
-    #         file.write("\tvirtual I_{packetName}Unit& get{packetName}Unit(const unsigned char & index) const = 0;".format(packetName=packetName))
-    #         # Closing class off
-    #         file.write("\n")
-    #         file.write("};")
-    #         file.close()
-    #     # Make the data unit file
-    #     headerFileName = "I_{packetName}Unit.h".format(packetName=packetName)
-    #     filePath = os.path.join(dataFolderNamePath,headerFileName)
-    #     with open(filePath, "w") as file:
-    #         file.write("#pragma once\n")
-    #         file.write('#include "../qtMethods.h"\n')
-    #         file.write("class I_{packetName}Unit:public QObject \n{{\nQ_OBJECT\n".format(packetName=packetName))
-    #         file.write("public:\n")  
-    #         # Deconstructor
-    #         file.write("\tvirtual ~I_{packetName}Unit() {{}};\n".format(packetName=packetName))
-    #         # Write Getter
-    #         for attribute in packetData:
-    #             # Setting Type
-    #             type = self.determineType(attribute)
-    #             # Ignore PackageID parse, maybe can remove if parsed json recieve will never send this
-    #             if(attribute["Name"] == "PackageID"):
-    #                 continue
-    #             # This package contains an enum to generate
-    #             if(attribute["enum"]):
-    #                 enumName = list(attribute["enum"].items())[0][0]
-    #                 listValues = list(attribute["enum"].items())[0][1]
-    #                 file.write("\t enum {enumName}\n{{\n".format(enumName=enumName))
-    #                 for object in listValues:
-    #                     key=(list(object.items())[0][0])
-    #                     value =(list(object.items())[0][1])
-    #                     file.write("\t{macroName}={macroValue},\n".format(macroName=value,macroValue=key))
-    #                 file.write("};\n")  
-    #             name = attribute["Name"].replace(" ", "")
-    #             file.write("\tvirtual {type} get{name}() const = 0;\n".format(type = type, name = name))
-    #         # Write out the setter for the function.
-    #         for attribute in packetData:
-    #             # Skip Package ID
-    #             if(attribute["Name"] == "PackageID"):
-    #                 continue
-    #             # Determine type for data
-    #             type = self.determineType(attribute)
-    #             name = attribute["Name"].replace(" ", "")
-    #             lname = name[:1].lower() + name[1:]
-    #             file.write("\tvirtual void set{name}(const {type}& {lname}) = 0;\n".format(type = type, name = name, lname=lname))
-    #         file.write("\n")
-    #         file.write("\n};")
+    def generateInterfaceForMultiUnits(self,packet):
+        packetName = packet
+        packetData = self.parsedData[packet]
+        # Make the folder Name
+        dataFolderNamePath = os.path.join(self.new_directory_path,packetName+"Data")
+        os.makedirs(dataFolderNamePath,exist_ok=True)
+        # Make the data interface file
+        headerFileName = "I_{packetName}Data.h".format(packetName=packetName)
+        filePath = os.path.join(dataFolderNamePath,headerFileName)
+        with open(filePath, "w") as file:
+            file.write("#pragma once\n")
+            file.write('#include <QObject>\n')
+            file.write('#include "I_{packetName}Unit.h"\n'.format(packetName = packetName)) 
+            file.write("class I_{packetName}Data:public QObject \n{{\nQ_OBJECT\n".format(packetName=packetName))
+            file.write("public:\n")  
+            # Deconstructor
+            file.write("\tvirtual ~I_{packetName}Data() {{}};\n".format(packetName=packetName))
+            # Standard 2 getters among multiple unit data packet.
+            file.write("\tvirtual unsigned char getNumberOfUnits() const = 0;\n")
+            file.write("\tvirtual I_{packetName}Unit& get{packetName}Unit(const unsigned char & index) const = 0;".format(packetName=packetName))
+            # Closing class off
+            file.write("\n")
+            file.write("};")
+            file.close()
+        # Make the data unit file
+        headerFileName = "I_{packetName}Unit.h".format(packetName=packetName)
+        filePath = os.path.join(dataFolderNamePath,headerFileName)
+        with open(filePath, "w") as file:
+            file.write("#pragma once\n")
+            file.write('#include <QObject>\n')
+            file.write("class I_{packetName}Unit:public QObject \n{{\nQ_OBJECT\n".format(packetName=packetName))
+            file.write("public:\n")  
+            # Deconstructor
+            file.write("\tvirtual ~I_{packetName}Unit() {{}};\n".format(packetName=packetName))
+            # Write Getter
+            for attribute in packetData:
+                # Setting Type
+                type = self.determineType(attribute)
+                # Ignore PackageID parse, maybe can remove if parsed json recieve will never send this
+                if(attribute["Name"] == "PackageID"):
+                    continue
+                # This package contains an enum to generate (At moment not required for MPPT or Motor Details)
+                if (attribute["Unit"] == "flag"):
+                    file.write("\tenum {name}\n\t{{\n\t".format(name=attribute["Name"]))
+                    for item in attribute["Detail"]:
+                        value = attribute["Detail"][item]
+                        file.write("\t{macroName} = {value},\n\t".format(macroName=item,value=value))
+                    file.write("\n\t};\n")
+                name = attribute["Name"].replace(" ", "")
+                file.write("\tvirtual {type} get{name}() const = 0;\n".format(type = type, name = name))
+            # Write out the setter for the function.
+            for attribute in packetData:
+                # Skip Package ID
+                if(attribute["Name"] == "PackageID"):
+                    continue
+                # Determine type for data
+                type = self.determineType(attribute)
+                name = attribute["Name"].replace(" ", "")
+                lname = name[:1].lower() + name[1:]
+                file.write("\tvirtual void set{name}(const {type}& {lname}) = 0;\n".format(type = type, name = name, lname=lname))
+            file.write("\n")
+            file.write("\n};")
             
     def generateInterface(self):
         for packet in self.parsedData:
             packetName = packet
             packetData = self.parsedData[packet]
             # Generate multiple data layout
-            if(packetData[0]["detail"]).get("Quantity",0) > 1:
+            if(packetData[0]["Detail"]).get("Quantity",0) > 1:
                 self.generateInterfaceForMultiUnits(packet)
             else:
                 # Make the folder Name
@@ -121,7 +107,7 @@ class DataLayer:
                 filePath = os.path.join(dataFolderNamePath,headerFileName)
                 with open(filePath, "w") as file:
                     file.write("#pragma once\n")
-                    file.write('#include "../qtMethods.h"\n')
+                    file.write('#include <QObject>\n')
                     file.write("class I_{packetName}Data:public QObject \n{{\nQ_OBJECT\n".format(packetName=packetName))
                     file.write("public:\n")
                     # Deconstructor
@@ -137,30 +123,29 @@ class DataLayer:
                         if(attribute["Name"] == "PackageID"):
                             continue
                         # This package contains an enum to generate
-                        # if(attribute["enum"]):
-                        #     enumName = list(attribute["enum"].items())[0][0]
-                        #     listValues = list(attribute["enum"].items())[0][1]
-                        #     file.write("\t enum {enumName}\n{{\n".format(enumName=enumName))
-                        #     for object in listValues:
-                        #         key=(list(object.items())[0][0])
-                        #         value =(list(object.items())[0][1])
-                        #         file.write("\t{macroName}={macroValue},\n".format(macroName=value,macroValue=key))
-                        #     file.write("};\n")    
-                        # If has bitflag, need to iterate through list in details and create proper functions.
-                        if(attribute["detail"]):
+                        if (attribute["Unit"] == "flag"):
+                            file.write("\tenum {name}\n\t{{\n\t".format(name=attribute["Name"]))
+                            for item in attribute["Detail"]:
+                                value = attribute["Detail"][item]
+                                file.write("\t{macroName} = {value},\n\t".format(macroName=item,value=value))
+                            file.write("\n\t};\n")
+
+                        if(attribute["Unit"] == "bitflag" and attribute["Detail"]):
                             # Motor number empty by default, and given a value when motor data packet is detected
                             motorNum=""
                             if (packetName == "MotorFaults"):
                                 # Get motor number
                                 motorNum = attribute["Name"][:2]
                             # iterate through detail
-                            for item in attribute["detail"]:
-                                value = list(item.values())[0] 
-                                noSpaceString = value.lstrip().replace(" ","")
-                                file.write("\tvirtual bool get{motorNum}{noSpaceString}() const = 0;\n".format(motorNum=motorNum,noSpaceString = noSpaceString))
+                            for item in attribute["Detail"]:
+                                value = attribute["Detail"][item]
+                                file.write("\tvirtual bool get{motorNum}{value}() const = 0;\n".format(motorNum=motorNum,value = value))
                             continue
                         # Write getter for main field
                         name = attribute["Name"].replace(" ", "")
+                        # Handle case if Unit is flag to use type as atribute name 
+                        if (attribute["Unit"] == "flag"):
+                            type=attribute["Name"]
                         file.write("\tvirtual {type} get{name}() const = 0;\n".format(type = type, name = name))
                     file.write("\n")
                     # Write out the setter for the function.
@@ -175,8 +160,9 @@ class DataLayer:
                     file.write("\n")
                     file.write("\n};")
 
-with open('output.json') as json_file:
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, 'output.json')
+with open(file_path) as json_file:
     data = json.load(json_file)
 test = DataLayer(json=data)
-test.genQMethods()
 test.generateInterface()
